a8a4a6a86806e966bce9989b07d5875c781c6577
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2024-10-29 22:32:56 +0100

    fix news pagination history

--- src/components/wp/Posts.jsx
+++ src/components/wp/Posts.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import Button from 'react-bootstrap/Button';
 import Pagination from 'react-bootstrap/Pagination';
@@ -37,7 +37,9 @@ function Posts({
     template = templates.list,
 }) {
     const [totalPages, setTotalPages] = useState(0);
-    const [activePage, setActivePage] = useState(1);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const activePage = location.state?.page ?? 1;
 
     const isAnalysis =
         categories.includes(wpCat.analyses) ||
@@ -72,7 +74,8 @@ function Posts({
     );
 
     const loadPage = (page) => () => {
-        setActivePage(page);
+        // navigate to the same page, just pass the current page via state object to preserve history
+        navigate(location.pathname, { state: { page } });
         scrollToTop();
     };
 
@@ -165,11 +168,6 @@ function Posts({
             }
         }
     }
-
-    // reset active page to 1 if search query changes
-    useEffect(() => {
-        setActivePage(1);
-    }, [search]);
 
     return (
         <div>
